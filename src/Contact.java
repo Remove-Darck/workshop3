a776a86dab47b59a93c68678b2b5fb9d0f7a47df
Remove-Darck <ghasiminooria@gmail.com>
2025-01-03 12:25:37 +0330

    u_here

---
+++ src/Contact.java
@@ -0,0 +1,50 @@
+import java.util.Objects;
+
+public class Contact {
+    private final String name, phoneNumber, address;
+    private final int age;
+
+
+    public Contact(String name, String phoneNumber, String address, int age) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+    @Override
+    public String toString(){
+        return "name: " + getName() + " | address: " + getAddress() + " | phone: " + getPhoneNumber() + "";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contact contact = (Contact) o;
+        return Objects.equals(name, contact.name) && Objects.equals(phoneNumber, contact.phoneNumber) && Objects.equals(address, contact.address);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, phoneNumber, address);
+    }
+}
+
---
+++ src/Phonebook.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class Phonebook {
+    private HashMap<String, Contact> contactHashMap;
+    private int numberOfContacts;
+
+    Phonebook(){
+        contactHashMap = new HashMap<>();
+        numberOfContacts = 0;
+    }
+
+    public boolean addContact(String name,  String phoneNumber, String address,int age){
+        Contact newContact = new Contact(name, phoneNumber, address, age);
+        ArrayList<Contact> contacts = new ArrayList<>(contactHashMap.values());
+        if (contacts.contains(newContact) ){
+            return false;
+        }
+        contactHashMap.put(name, newContact);
+        numberOfContacts++;
+        return true;
+    }
+
+    public void printContacts(){
+        ArrayList<Contact> contacts = new ArrayList<>(contactHashMap.values());
+        System.out.println("\nprinting contacts:");
+        for(Contact el : contacts){
+
+            System.out.println(el);
+            System.out.print("--------------------------------------------------------------\n");
+        }
+        return;
+    }
+    public Contact getContact(String name){
+        return contactHashMap.get(name);
+    }
+
+    public int getAvgAge(){
+        ArrayList<Contact> contacts = new ArrayList<>(contactHashMap.values());
+        double sum = 0.00001;
+        for(Contact el : contacts){
+            System.out.println(el);
+            sum += el.getAge();
+        }
+        return (int) Math.round(sum / contacts.size());
+    }
+}
